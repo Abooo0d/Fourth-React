715c99790232c7755ab6394bba58cc9482d571ff
Aboo00d <abdsadalden2001@gmail.com>
2023-12-23 00:02:58 +0200

    Creating The Footer Section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
-import './Newsletter.css';
+import React from "react";
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsLetter">
+    <div className="app__newsLetter-heading">
+      <SubHeading title="News Letter" />
+      <h1 className="headtext__cormorant">Subscribe To Our NewsLetter</h1>
+      <p className="p__opensans">And Never Miss Latest Updates</p>
+    </div>
+
+    <div className="app__newsLetter-input flex__center">
+      <input type="email" name="Email" placeholder="Enter Yore Email" />
+      <button type="button" className="custom__button">
+        Subscribe
+      </button>
+    </div>
   </div>
 );
 
--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
-
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "1rem" }}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans" style={{}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+          iusto? Magni saepe totam, voluptate delectus exercitationem veniam ab
+          quos ducimus.
+        </p>
+        <p
+          className="p__opensans"
+          style={{ color: "#dcca87", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__cormorant">Lorem ipsum dolor sit amet.</p>
+        <p className="p__cormorant">Lorem ipsum dolor sit amet.</p>
+      </div>
+      <button
+        type="button"
+        className="custom__button"
+        style={{ marginTop: "2rem" }}
+      >
+        Visit Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="Find Us" />
+    </div>
   </div>
 );
 
